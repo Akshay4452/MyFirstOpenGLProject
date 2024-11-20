1f8a4f436f27a51aa1a012527ab433eb246c33b3
akshay_manthekar <akky123654@gmail.com>
2024-11-20 16:11:17 +0530

    Using Vertex Array
    Vertex array objects store the vertex attribute configuration/state and also information about which vertex buffer has what type of layout.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -146,6 +146,10 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Learning OpenGL", NULL, NULL);
     if (!window)
@@ -178,13 +182,20 @@ int main(void)
         2, 3, 0   // Indices of positions to create second triangle
     };
 
+    // Vertex Array Object - Need to explicitly define if we are using opengl core profile
+    unsigned int vao;
+    GLCall(glGenVertexArrays(1, &vao));
+    GLCall(glBindVertexArray(vao));
+
     unsigned int buffer;
     GLCall(glGenBuffers(1, &buffer));
     GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
     GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
 
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    GLCall(glEnableVertexAttribArray(0));
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
+
+    // Note: glVertexAttribPointer() automatically binds the vertex buffer with vertex array
 
     // Generating and binding index buffer
     unsigned int ibo;  // ibo = index buffer object
@@ -210,6 +221,11 @@ int main(void)
 
     /* NOTE: glUniform4f() must be called before glDrawArray() call */
 
+    // Unbound everything
+    GLCall(glUseProgram(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
     float r = 0.0f;
     float increment = 0.05f;
 
@@ -222,7 +238,15 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        GLCall(glUseProgram(shader));
         GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
+        // Bind vertex array
+        GLCall(glBindVertexArray(vao));
+
+        // Index Buffer
+        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
         if (r > 1.0f)
